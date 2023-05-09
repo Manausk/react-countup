fa08e68400655766bf9812060d847af1782fc3e9
ManausK <1905246@kiit.ac.in>
2023-05-09 09:05:12 +0530

    react-countup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CountUp from "react-countup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>
+      <div className="App">
+        <CountUp end={200} />
+        <br />
+        <CountUp end={200} duration={10} />
+        <br />
+        <CountUp start={700} end={1200} duration={5} />
+      </div>
+    </h1>
   );
 }
 
